3be6788b4faf2c17fc0320229b548a520bb26275
Awadh-Awadh <awadhawadh84@yahoo.com>
2021-11-23 14:39:47 +0300

    add styles to the details page

--- main/views.py
+++ main/views.py
@@ -7,8 +7,9 @@ def home(request):
    return render(request, 'main/main.html')
 
 def info(request):
-   data = api_request('muranga')
+   data = api_request('nairobi')
    context = {
-      'data': data['weather']
+      'data': data['weather'],
+      'city': data['name']
    }
    return render(request, 'main/data.html', context)
